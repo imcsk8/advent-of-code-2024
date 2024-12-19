bc3b5f83ee24f939f539f545b53df8c4fada0988
Iván Chavero <ichavero@chavero.com.mx>
2024-12-19 15:55:55 -0600

    Day 1 puzzle 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use sorting_rs;
+use std::collections::HashMap;
 
 static INPUT_DATA: &str = "../data/input";
 
@@ -7,6 +8,7 @@ fn main() {
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
     let mut distance: Vec<i32> = Vec::new();
+    let mut appearances: HashMap<i32, i32> = HashMap::new();
     for line in fs::read_to_string(INPUT_DATA).unwrap().lines() {
         let mut iter = line.split_ascii_whitespace();
         left.push(iter.next().unwrap().parse::<i32>().unwrap());
@@ -16,11 +18,32 @@ fn main() {
     sorting_rs::quick_sort(&mut left);
     sorting_rs::quick_sort(&mut right);
     let mut total = 0;
+    let mut total_similarity = 0; 
     for (i, _) in left.iter().enumerate() {
+        let count = count_right(left[i], &right, &mut appearances);
+        let similarity = left[i] * count;
         let dist = (left[i] - right[i]).abs();
         total += dist;
+        total_similarity += similarity;
         distance.push(dist);
-        println!("LEFT: {}, RIGHT: {}, DISTANCE: {}", left[i], right[i], dist);
+        println!("LEFT: {}, RIGHT: {}, DISTANCE: {}, COUNT: {}, SIMILARITY: {}",
+            left[i], right[i], dist, count, similarity);
     }
     println!("Total Distance: {}", total);
+    println!("Total Similarity: {}", total_similarity);
+}
+
+// This is going to be very slow
+fn count_right(left_elem: i32, right: &Vec<i32>, ap: &mut HashMap<i32, i32>) -> i32 {
+    let mut count = 0;
+    for r in right {
+        if ap.contains_key(&left_elem) {
+            return *ap.get(&left_elem).unwrap();
+        }
+        if left_elem == *r {
+            count += 1;
+        }
+    }
+    ap.insert(left_elem, count);
+    count
 }
